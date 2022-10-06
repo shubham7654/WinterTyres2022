d2df0e9859643c0a58aed0d57cb8efe79855cdce
Shubham Patel <shubhampatel7654@gmail.com>
2022-10-06 17:56:35 +0530

    Worked on green cell.

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+$td = $("td:not([class])");
 $redCell = $(".red-cell");
 
-$redCell.attr({"data-bs-toggle": "modal", "data-bs-target": "#exampleModal"});
+$redCell.attr({ "data-bs-toggle": "modal", "data-bs-target": "#exampleModal" });
+
+$td.on("click", function () {
+  $td.css("backgroundColor", "#92d050")
+  $(this).css("backgroundColor", "rgb(255,219,0)");
+});
